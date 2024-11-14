3af56ba13bfc6090f7fdef5de3afb5eda6e8115c
gehad2789 <gehad2784377@gmail.com>
2024-11-14 08:04:24 +0200

    added new component

--- Guzel/src/app/app.component.ts
+++ Guzel/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./Layouts/header/header.component";
+import { CatNavComponent } from "./Layouts/cat-nav/cat-nav.component";
+import { FooterComponent } from "./Layouts/footer/footer.component";
+import { PostCardComponent } from "./Layouts/post-card/post-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CatNavComponent, FooterComponent, PostCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Guzel/src/app/app.routes.ts
+++ Guzel/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { PostComponent } from './Components/post/post.component';
+import { TermsCondComponent } from './Components/terms-cond/terms-cond.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { SubscriptionLog } from 'rxjs/internal/testing/SubscriptionLog';
+import { HeaderComponent } from './Layouts/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},//defult
+    {path:'contact-us',component:ContactUsComponent},
+    {path:'post',component:PostComponent},
+    {path:'terms&conditions',component:TermsCondComponent},
+    {path:'subscriptionForm',component:SubscriptionFormComponent},
+    {path:'header',component:HeaderComponent},
+
+
+];
